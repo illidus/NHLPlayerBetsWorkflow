961d76750a74cd625423a2f150679f4bfe0472d3
illidus <ryaneyre1337@gmail.com>
2026-01-11 22:53:09 -0800

    chore: remove stale sandbox debug scripts

--- sandbox/debug_panarin_props.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data/raw/nhl_player_props_all.csv')
-subset = df[df['Player'].str.contains('Artemi Panarin', case=False, na=False)]
-
-print("--- Artemi Panarin Props ---")
-print(subset[['Player', 'Market', 'Sub_Header', 'Odds_1', 'Odds_2']].to_string(index=False))
